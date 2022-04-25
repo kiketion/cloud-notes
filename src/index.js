29779a3c85589c04d0988abdd4c493c482123598
kiketion <kiketionje@gmail.com>
2022-04-25 12:38:52 +0300

    socket.io configured

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import app from './app';
+import { Server as WebsocketServer } from 'socket.io';
+import http from 'http';
+
+const server = http.createServer(app);
+const httpServer = server.listen(3000);
+const io = new WebsocketServer(httpServer);
+
+console.log('Server is running on port 3000');

5c2aa81c2aecd7a91f34c08982b8b21385778940
kiketion <kiketionje@gmail.com>
2022-04-27 15:28:39 +0300

    db

---
+++ src/public/main.js
@@ -0,0 +1,8 @@
+import { loadNotes } from './socket.js';
+import { onHandleSubmit, renderNotes } from './ui.js';
+
+loadNotes(renderNotes);
+
+const noteForm = document.querySelector('#noteForm');
+
+noteForm.addEventListener('submit', onHandleSubmit);

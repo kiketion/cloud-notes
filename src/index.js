6a0af4217c40020960fd05b6e7cba828e04f67c5
kiketion <kiketionje@gmail.com>
2022-04-27 10:32:00 +0300

    mongo, dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import app from './app';
 import { Server as WebsocketServer } from 'socket.io';
 import http from 'http';
 
+import { connectDB } from './db';
+
+connectDB();
+
 const server = http.createServer(app);
 const httpServer = server.listen(3000);
 const io = new WebsocketServer(httpServer);

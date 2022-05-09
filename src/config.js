20b7b0c20b27e79c524fd875c97f7a3826d6857a
kiketion <kiketionje@gmail.com>
2022-05-10 00:35:28 +0300

    port config

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ import { config } from 'dotenv';
 config();
 
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import app from './app';
 import { Server as WebsocketServer } from 'socket.io';
 import http from 'http';
 import sockets from './sockets';
+import { PORT } from './config';
 
 import { connectDB } from './db';
 
 connectDB();
 
 const server = http.createServer(app);
-const httpServer = server.listen(3000);
-console.log('Server is running on port 3000');
+const httpServer = server.listen(PORT);
+console.log('Server is running on port', PORT);
 
 const io = new WebsocketServer(httpServer);
 sockets(io);
